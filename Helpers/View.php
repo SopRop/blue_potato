b20e2c5454928ef984086d23a84c21da1a6debf8
Leamundis <jonathan.peraudon@yahoo.fr>
2018-10-24 07:35:35 +0000

    Twig implemented with a helper to simplify views call

---
+++ Helpers/View.php
@@ -0,0 +1,14 @@
+<?php
+namespace Helper;
+use \Twig_Loader_Filesystem;
+use \Twig_Environment;
+
+class View
+{
+    static public function twig($view, $arg) {
+        $loader = new Twig_Loader_Filesystem($_SERVER['DOCUMENT_ROOT']);
+        $twig = new Twig_Environment($loader);
+    
+        echo $twig->render($view, array('users' => $arg, 'title' => 'Salariers'));
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,4 +1,7 @@
 <?php
+use Helper\View as View;
+require_once __DIR__ . "/../vendor/autoload.php";
 
-require_once __DIR__ . '/../vendor/autoload.php';
-echo 'yeah!';
+$arr = [['name' => 'Jon','age' => 5], ['name' => 'Jon','age' => 5], ['name' => 'Jon','age' => 5]];
+
+View::twig('user.twig.html', $arr);
