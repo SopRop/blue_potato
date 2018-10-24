c3fe888550e265dcf8a6ea47141ad9722dea0dcf
Leamundis <jonathan.peraudon@yahoo.fr>
2018-10-24 13:38:01 +0000

    Cleaning index

--- public/index.php
+++ public/index.php
@@ -5,11 +5,4 @@ $src = __DIR__ . '/../';
 require_once $src . 'vendor/autoload.php';
 
 Core\Config::broadcast($src);
-//Core\Router::route();
-
-
-use \Model\Worker;
-
-$worker = new Worker();
-
-Worker::getAll();
+Core\Router::route();
