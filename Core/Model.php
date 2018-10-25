1d6b4b2376994b44b1d5041ea023b3bf6e391bb6
SopRop <sop.ropert@gmail.com>
2018-10-25 17:19:09 +0200

    link between table and model

--- Core/Model.php
+++ Core/Model.php
@@ -4,12 +4,16 @@ use \Core\Access;
 
 abstract class Model
 {
-    public $table;
+    protected $table;
+
     public function __construct()
     {
         $className = static::class;
         $classNames = explode('\\', $className);
-        $this->table = strtolower($classNames[1]);
+
+        if (!isset($this->table)) {
+            $this->table = strtolower($classNames[1]);
+        }
     }
     
     static public function __callStatic($methodName, $arg)
@@ -19,7 +23,6 @@ abstract class Model
         $access = new Access;
         if (method_exists($access, $methodName)) {
             $args = array_merge([$model->table], $arg);
-            var_dump($args);
             $result = call_user_func_array([$access, $methodName], $args);
             return $result;
         }
